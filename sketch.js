63fd58b71df2c4032da085dad25661d365d91353
E. Dronkert <ednl@users.noreply.github.com>
2021-05-25 15:47:04 +0200

    Eliminate separate binstr function

--- sketch.js
+++ sketch.js
@@ -2,11 +2,11 @@
 // https://adventofcode.com/2017/day/14
 // Solution by @ednl https://github.com/ednl
 
-const key = "hxtvlmkl-";  // my puzzle input
-const disksize = 128;
-const gridsize = 5;
-const maxhue = 360;
-const speedup = 5;
+const key = "hxtvlmkl";  // my puzzle input
+const disksize = 128;    // width and height of the grid
+const gridsize = 5;      // pixel width and height of grid cells
+const maxhue = 360;      // hue cycles through how many values
+const speedup = 5;       // do how many regions per draw() loop (>= 1)
 
 let disk;
 let x = 0;
@@ -15,18 +15,6 @@ let regions = 0;
 let hue = 0;
 let stack = [];
 let divRegions;
-
-// Convert value to bit string
-function bin(val, len)
-{
-    let s = "";
-    let bit = 1 << (len - 1);
-    while (bit) {
-        s += (val & bit) ? '1' : '0';
-        bit >>= 1;
-    }
-    return s;
-}
 
 // Hashing function from AoC 2017 day 10
 // https://adventofcode.com/2017/day/10
@@ -62,30 +50,36 @@ function knot(lenstr)
             skip++;
         }
     }
-    let dense = "";
+    let a = new Array(16 * 8);  // 16 bytes
+    let k = 0;
     for (let i = 0; i < hlen; i += 16) {
         let x = 0;
         for (let j = 0; j < 16; ++j) {
           x ^= hash[i + j];
         }
-        dense += bin(x, 8);
-    }
-    let a = new Array(dense.length);
-    for (let i = 0; i < dense.length; ++i) {
-        a[i] = dense[i] == '1' ? 1 : 0;
+        let bit = 128;  // 8-bit conversion
+        while (bit) {
+            a[k++] = x & bit ? 1 : 0;
+            bit >>= 1;
+        }
     }
     return a;
 }
 
 function setup()
 {
+    // Screen layout
     createCanvas(disksize * gridsize, disksize * gridsize);
     strokeWeight(1);
 
     let divTitle = createDiv('Advent of Code 2017 Day 14: Disk Defragmentation');
     let divBits = createDiv('Bits: 0');
     divRegions = createDiv('Regions: 0');
-    let divLink = createDiv('<br /><a href="https://adventofcode.com/2017/day/14" target="_blank">puzzle</a> | <a href="https://github.com/ednl/defrag/blob/main/sketch.js" target="_blank">code</a> | <a href="https://ednl.github.io/" target="_blank">github</a> | <a href="https://twitter.com/ednl" target="_blank">twitter</a>');
+    let divLink = createDiv(
+        '<br /><a href="https://adventofcode.com/2017/day/14" target="_blank">puzzle</a>'
+        + ' | <a href="https://github.com/ednl/defrag/blob/main/sketch.js" target="_blank">code</a>'
+        + ' | <a href="https://ednl.github.io/" target="_blank">github</a>'
+        + ' | <a href="https://twitter.com/ednl" target="_blank">twitter</a>');
     divTitle.style('font', 'bold 22px Verdana');
     divBits.style('font', '20px Verdana');
     divRegions.style('font', '20px Verdana');
@@ -95,16 +89,18 @@ function setup()
     disk = new Array(disksize);
     let bits = 0;
     for (let i = 0; i < disksize; ++i) {
-        s = key + i;
+        s = key + '-' + i;
         disk[i] = knot(s);
         for (let j = 0; j < disksize; ++j) {
             bits += disk[i][j];
         }
     }
+    // Updating while calculating each row not necessary because too quick
+    // *and* not possible here anyway, because no draw() looping yet
     divBits.html('Part 1: bits on disk = ' + bits);
     console.log("Part 1: bits on disk = " + bits);
 
-    // Grid
+    // Draw grid
     stroke(0);
     noFill();
     for (let i = 0; i <= disksize; ++i) {
@@ -113,7 +109,7 @@ function setup()
         line(p, 0, p, disksize * gridsize);
     }
 
-    // Pattern
+    // Draw pattern (seperately because different settings)
     noStroke();
     fill(0);
     for (let i = 0; i < disksize; ++i) {
@@ -125,6 +121,8 @@ function setup()
             }
         }
     }
+
+    // Set colour mode for linear hue cycling
     colorMode(HSB, maxhue, 100, 100);
 }
 
@@ -133,12 +131,12 @@ function draw()
 {
     for (let loop = 0; loop < speedup; ++loop) {
         if (stack.length) {
-            // Fill in the current region
+            // Fill in the whole region with DFS algorithm
             while (stack.length) {
                 const coor = stack.pop();
                 const i = coor[0];
                 const j = coor[1];
-                disk[i][j] = 0;
+                disk[i][j] = 0;  // prevent this cell from being used again
                 rect(j * gridsize, i * gridsize, gridsize, gridsize);
                 if (i > 0 && disk[i - 1][j] == 1) {
                     stack.push([i - 1, j]);
@@ -154,20 +152,20 @@ function draw()
                 }
             }
         } else if (y < disksize) {
-            // Find the next region
+            // Find the next separate region
             while (y < disksize && disk[y][x] != 1) {
-              x++;
-              if (x == disksize) {
-                  x = 0;
-                  y++;
-              }
+                x++;
+                if (x == disksize) {
+                    x = 0;
+                    y++;
+                }
             }
             if (y < disksize && disk[y][x] == 1) {
+                // Next region found, give update
                 regions++;
                 divRegions.html('Part 2: regions on disk = ' + regions);
                 stack.push([y, x]);
-                fill(hue, 100, 100);
-                hue++;
+                fill(hue++, 100, 100);
                 if (hue == maxhue) {
                     hue = 0;
                 }
